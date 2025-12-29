146dba362f159a1298cbb9b871688d70f1400e03
Christy Chovalloor <christy.chovalloor@gmail.com>
2025-12-29 23:06:46 +0000

    RESTRUCTURING

---
+++ ATLAS-IE (Intelligence Engine)/tests/testEngine.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from src.core import MTSEngine
+
+df_raw = pd.read_csv("01_Data/Canonical/lewisHamilton_AbuDhabi_2021.csv")
+engine = MTSEngine(window=5, sensitivity="normal")
+# Explicitly select the signal (example: LapTime)
+df = df_raw[["LapTime"]]
+
+out = engine.run(df)
+result = engine.results_schema(out)
+print(result.to_string())
